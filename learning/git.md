83f88f33221d123f9419c915f8ddfaf3ecd72500
hlt <xiangruyimo0223@163.com>
2020-02-05 11:53:40 +0800

    20200205001

--- learning/git.md
+++ learning/git.md
@@ -13,4 +13,5 @@ $ git config --global user.email example@hlt.com
 ```
 3. 
 git clone git@github.com:hlt7777777/hlt.github.io.git
+
 git push
---
+++ learning/practice.py
@@ -0,0 +1,19 @@
+
+# name = input('please input your name:')
+print('name')
+
+print()
+
+print('dsdf dsdfsf dfsf\ndfsf ')
+
+print('hello')
+print('world')
+first_name = 'hello'
+last_name = 'world'
+print('my ' + first_name.capitalize() + ' ' + last_name.capitalize())
+
+print( first_name + last_name )
+
+
+
+
