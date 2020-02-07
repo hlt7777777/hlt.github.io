8f1952591cab679942ad4bdaacf37a9aae6e1fea
hlt <xiangruyimo0223@163.com>
2020-02-07 10:27:50 +0800

    20200207001

--- learning/practice.py
+++ learning/practice.py
@@ -13,7 +13,20 @@ last_name = 'world'
 print('my ' + first_name.capitalize() + ' ' + last_name.capitalize())
 
 print( first_name + last_name )
+output = 'my {0}, {1}'.format(first_name, last_name)
+print(output)
 
+output1 = f'it is {first_name} - {last_name}'
+print(str(9969).replace('6', '9', 1))
 
+a = [[1,2,3],[4,5,6],[7,8,9]]
+
+from datetime import datetime,timedelta
+
+today = datetime.now()
+days = timedelta(days = 1)
+
+yesterday = today - days 
+print(today,yesterday)
 
 
