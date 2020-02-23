6955435ddf6f8752c147d58e7f76ecbf695e3c33
hlt <xiangruyimo0223@163.com>
2020-02-23 21:44:05 +0800

    20200223

--- learning/practice.py
+++ learning/practice.py
@@ -1,38 +1,68 @@
 
-# name = input('please input your name:')
-print('name')
+# # name = input('please input your name:')
+# print('name')
 
-print()
+# print()
 
-print('dsdf dsdfsf dfsf\ndfsf ')
+# print('dsdf dsdfsf dfsf\ndfsf ')
 
-print('hello')
-print('world')
-first_name = 'hello'
-last_name = 'world'
-print('my ' + first_name.capitalize() + ' ' + last_name.capitalize())
+# print('hello')
+# print('world')
+# first_name = 'hello'
+# last_name = 'world'
+# print('my ' + first_name.capitalize() + ' ' + last_name.capitalize())
 
-print( first_name + last_name )
-output = 'my {0}, {1}'.format(first_name, last_name)
-print(output)
+# print( first_name + last_name )
+# output = 'my {0}, {1}'.format(first_name, last_name)
+# print(output)
 
-output1 = f'it is {first_name} - {last_name}'
-print(str(9969).replace('6', '9', 1))
+# output1 = f'it is {first_name} - {last_name}'
+# print(str(9969).replace('6', '9', 1))
 
-a = [[1,2,3],[4,5,6],[7,8,9]]
+# a = [[1,2,3],[4,5,6],[7,8,9]]
 
-from datetime import datetime,timedelta
+# from datetime import datetime,timedelta
 
-today = datetime.now()
-days = timedelta(days = 1)
+# today = datetime.now()
+# days = timedelta(days = 1)
 
-yesterday = today - days 
-print(today,yesterday)
+# yesterday = today - days 
+# print(today,yesterday)
 
-one_week = timedelta(weeks=1)
-last_week = today - one_week
-print(last_week)
+# one_week = timedelta(weeks=1)
+# last_week = today - one_week
+# print(last_week)
 
-birthday = input('input your date(dd/mm/yyyy)')
-birthday_day = datetime.strptime(birthday, '%d/%m/%Y')
-print(str(birthday_day))
+# birthday = input('input your date(dd/mm/yyyy)')
+# birthday_day = datetime.strptime(birthday, '%d/%m/%Y')
+# print(str(birthday_day))
+
+# x = 420 
+# y = 0 
+
+# try:
+#     print(x / y)
+# except ZeroDivisionError as e:
+#     print('zero')
+# else:
+#     print('ture')
+# finally:
+#     print('finally')
+
+# princ = 0.8
+# if princ >= 1.0:
+#     tax = 0.7
+# else:
+#     tax = 1
+
+# print(tax)
+
+# a = 1
+# if a == 1:
+#     print(12)
+# elif a == 2:
+#     print(17)
+
+
+
+print(6281+12054+9560)
